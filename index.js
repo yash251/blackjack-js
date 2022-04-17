048e311d1c7c75b95114ec4e5f90a9a11021825b
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-17 22:10:30 +0530

    display cards

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
+let firstCard = 10;
+let secondCard = 4;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
 
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-console.log(sumEl)
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
+    cardsEl.textContent = "Cards :" + firstCard + ", " +secondCard;
     sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
