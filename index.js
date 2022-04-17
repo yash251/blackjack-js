a2477098f1b1ee5221e68174e03fbbedb4ebb909
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-17 22:26:19 +0530

    startgame -> rendergame

--- index.js
+++ index.js
@@ -10,22 +10,28 @@ let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
+    renderGame();
+}
+
+function renderGame() {
     cardsEl.textContent = "Cards :" + firstCard + ", " +secondCard;
     sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
-        message = "Do you want to draw a new card?"
+        message = "Do you want to draw a new card?";
     } 
     else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
     } 
     else {
-        message = "You're out of the game!"
-        isAlive = false
+        message = "You're out of the game!";
+        isAlive = false;
     }
-    messageEl.textContent = message
+    messageEl.textContent = message;
 }
 
 function newCard() {
-    
+    let card = 6;
+    sum += card;
+    renderGame();
 }
