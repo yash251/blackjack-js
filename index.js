a6ab073cb9b54803374d4f27d2725e4bd04c3369
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-18 22:25:32 +0530

    randomcard boilerplate

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 10;
-let secondCard = 4;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
@@ -9,6 +9,10 @@ let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+
+function getRandomCard() {
+    return 7;
+}
 
 function startGame() {
     renderGame();
@@ -35,7 +39,7 @@ function renderGame() {
 }
 
 function newCard() {
-    let card = 6;
+    let card = getRandomCard();
     sum += card;
     cards.push(card);
     renderGame();
